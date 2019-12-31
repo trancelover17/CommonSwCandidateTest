c92498bd0634181b1c723047e350c54d213a60c9
Vyacheslav Sorokin <sorokinslava95@gmail.com>
2019-12-31 12:13:07 +0300

    code moved here

---
+++ CommonSwCandidateTest/BaseItemComparer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using CommonSwCandidateTest.Data;
+
+namespace CommonSwCandidateTest_2
+{
+    public class BaseItemComparer : IComparer<BaseItem>
+    {
+        public int Compare(BaseItem a, BaseItem b)
+        {
+            // first, we compare by 'PlannedStart'
+            int result = DateTime.Compare(a.PlannedStart, b.PlannedStart);
+            if (result == 0)
+            { // if previous comparison returns '0' - dates are equal
+                result = DateTime.Compare(a.PlannedEnd, b.PlannedEnd); // then compare by 'PlannedEnd'
+                if (result == 0) // if previous comparison returns '0' - dates are equal
+                    result = string.Compare(a.Name, b.Name); // and then compare by 'Name'
+            }
+            return result;
+        }
+    }
+}
